eadf421edb383959dd2c8c29668d2cf5c2cf516f
Vitalij Ryndin <crashmax2015@mail.ru>
2020-09-18 16:44:58 +0800

    initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WinForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.CustomPanel = new System.Windows.Forms.Label();
+            this.MaxMinButton = new System.Windows.Forms.Button();
+            this.MinButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CustomPanel
+            // 
+            this.CustomPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.CustomPanel.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CustomPanel.Location = new System.Drawing.Point(0, 0);
+            this.CustomPanel.Name = "CustomPanel";
+            this.CustomPanel.Size = new System.Drawing.Size(400, 21);
+            this.CustomPanel.TabIndex = 4;
+            this.CustomPanel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CustomPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMove);
+            // 
+            // MaxMinButton
+            // 
+            this.MaxMinButton.BackColor = System.Drawing.SystemColors.Control;
+            this.MaxMinButton.Enabled = false;
+            this.MaxMinButton.FlatAppearance.BorderSize = 0;
+            this.MaxMinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MaxMinButton.Image = global::WinForms.Properties.Resources.max_gray;
+            this.MaxMinButton.Location = new System.Drawing.Point(350, 0);
+            this.MaxMinButton.Name = "MaxMinButton";
+            this.MaxMinButton.Size = new System.Drawing.Size(25, 21);
+            this.MaxMinButton.TabIndex = 5;
+            this.MaxMinButton.UseVisualStyleBackColor = false;
+            this.MaxMinButton.Click += new System.EventHandler(this.ToggleMaximize);
+            // 
+            // MinButton
+            // 
+            this.MinButton.BackColor = System.Drawing.SystemColors.Control;
+            this.MinButton.FlatAppearance.BorderSize = 0;
+            this.MinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinButton.Image = ((System.Drawing.Image)(resources.GetObject("MinButton.Image")));
+            this.MinButton.Location = new System.Drawing.Point(325, 0);
+            this.MinButton.Name = "MinButton";
+            this.MinButton.Size = new System.Drawing.Size(25, 21);
+            this.MinButton.TabIndex = 1;
+            this.MinButton.UseVisualStyleBackColor = false;
+            this.MinButton.Click += new System.EventHandler(this.MinApp);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.SystemColors.Control;
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Image = global::WinForms.Properties.Resources.close_gray;
+            this.CloseButton.Location = new System.Drawing.Point(375, 0);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(25, 21);
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.UseVisualStyleBackColor = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseApp);
+            this.CloseButton.MouseLeave += new System.EventHandler(this.CloseButtonLeave);
+            this.CloseButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.CloseButtonHover);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.MaxMinButton);
+            this.Controls.Add(this.MinButton);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.CustomPanel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Opacity = 0D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.LoadApp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button MinButton;
+        private System.Windows.Forms.Label CustomPanel;
+        private System.Windows.Forms.Button MaxMinButton;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoadApp(object sender, EventArgs e)
+        {
+            for (Opacity = 0; Opacity < 0.95; Opacity += 0.05)
+            {
+                await Task.Delay(10);
+            }
+        }
+
+        string WinLocation()
+        {
+            string x = Location.X.ToString();
+            string y = Location.Y.ToString();
+
+            return $"{x}, {y}";
+        }
+
+        private void FormMove(object sender, MouseEventArgs e)
+        {
+            CustomPanel.Capture = false;
+            Message m = Message.Create(Handle, 161, new IntPtr(2), IntPtr.Zero);
+            WndProc(ref m);
+            WinLocation();
+        }
+
+        private void CloseApp(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void MinApp(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void ToggleMaximize(object sender, EventArgs e)
+        {
+            WindowState = WindowState == FormWindowState.Maximized ? FormWindowState.Normal : FormWindowState.Maximized;
+        }
+
+        private void CloseButtonHover(object sender, MouseEventArgs e)
+        {
+            CloseButton.BackColor = Color.FromArgb(233, 37, 57);
+            CloseButton.Image = Properties.Resources.close_white;
+        }
+
+        private void CloseButtonLeave(object sender, EventArgs e)
+        {
+            CloseButton.BackColor = SystemColors.Control;
+            CloseButton.Image = Properties.Resources.close_gray;
+        }
+
+        /* resize window */
+
+        private bool resizing = false;
+        private Point last = new Point(0, 0);
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!resizing)
+            {
+                bool resize_x = e.X > (Width - 8);
+                bool resize_y = e.Y > (Height - 8);
+
+                if (resize_x && resize_y)
+                    Cursor = Cursors.SizeNWSE;
+                else if (resize_x)
+                    Cursor = Cursors.SizeWE;
+                else if (resize_y)
+                    Cursor = Cursors.SizeNS;
+                else Cursor = Cursors.Default;
+            }
+            else
+            {
+                int w = Size.Width;
+                int h = Size.Height;
+
+                if (Cursor.Equals(Cursors.SizeNWSE))
+                {
+                    Size = new Size(w + (e.Location.X - last.X), h + (e.Location.Y - last.Y));
+                    ResizePanel(e);
+                }
+                else if (Cursor.Equals(Cursors.SizeWE))
+                {
+                    Size = new Size(w + (e.Location.X - last.X), h);
+                    ResizePanel(e);
+                }
+                else if (Cursor.Equals(Cursors.SizeNS))
+                {
+                    Size = new Size(w, h + (e.Location.Y - last.Y));
+                }
+
+                last = e.Location;
+            }
+        }
+
+        private void ResizePanel(MouseEventArgs e)
+        {
+            CloseButton.Location = new Point(CloseButton.Location.X + (e.Location.X - last.X), 0);
+            MaxMinButton.Location = new Point(MaxMinButton.Location.X + (e.Location.X - last.X), 0);
+            MinButton.Location = new Point(MinButton.Location.X + (e.Location.X - last.X), 0);
+            CustomPanel.Size = new Size(e.Location.X, 21);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            resizing = true;
+            last = e.Location;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            resizing = false;
+        }
+
+        /* resize window */
+    }
+}
